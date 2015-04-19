955f2041172973d0fd2ff371590e23d0cbe59fc3
Stefan Fidanov <fidanov@gmail.com>
2015-04-19 11:56:18 +0300

    Adds body-parser for parsing POST requests data and update packages.
    In addition it also uses a port supplied by the environment.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express')
   , app = express()
+  , port = process.env.PORT || 3000
 
 app.engine('jade', require('jade').__express)
 app.set('view engine', 'jade')
 
 app.use(express.static(__dirname + '/public'))
+app.use(require('body-parser').json())
 app.use(require('./controllers'))
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000...')
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
 })
